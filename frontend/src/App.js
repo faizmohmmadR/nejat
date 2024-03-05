ff77b641455d63e0f38427509ffb659311cf700e
Tahir <thair@gmail.com>
2024-03-05 18:04:32 +0430

    contact page is created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,6 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
 import Contact from "./Components/contact";
-=======
-
-
-import ContactPage from "./Components/contact";
-
-// import Login from "./Components/Login";
-import About from "./Components/About";
-
-import Registration from "./Components/User/Registration";
-
-import ContactPage from "./Components/contact";
-
-
->>>>>>> 44868f6b40a21e5fef1dfbc218fdf86581da41cd
 function App() {
   return (
     <div>
