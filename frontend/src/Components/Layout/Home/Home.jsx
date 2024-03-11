d51a8db6e468282d5c45fcd6f7d9170e8981f5ae
Faiz Mohammad <faizmohammadramdel@gmail.com>
2024-03-11 17:58:17 +0430

    home updated

--- frontend/src/Components/Layout/Home/Home.jsx
+++ frontend/src/Components/Layout/Home/Home.jsx
@@ -1,8 +1,17 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
 import React from "react";
 import Services from "../Services/Services";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const Home = () => {
+  const data = [
+    { id: 1, title: " Rapid Aid" },
+    { id: 2, title: "Efficient Support" },
+    { id: 3, title: "Safe Evacuations" },
+    { id: 4, title: "Shelter Provision" },
+    { id: 5, title: "Life-saving Missions" },
+    { id: 6, title: "Risk Reduction" },
+  ];
   return (
     <Grid sx={{ margin: "0px auto" }} maxWidth="xl">
       <Grid
@@ -11,7 +20,6 @@ const Home = () => {
         sx={{
           background: (theme) =>
             `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.8)), url('img/d.jpg')`,
-          backgroundAttachment: "fixed",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
@@ -29,101 +37,94 @@ const Home = () => {
       </Grid>
 
       <Services />
-      <Grid
-        container
-        margin="50px 0px"
-        sx={{
-          background: (theme) =>
-            `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.5)), url('img/d.jpg')`,
-          backgroundAttachment: "fixed",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          color: "white",
-        }}
-      >
+      <Grid container justifyContent="center" gap={5} margin={"50px 0px"}>
+        <Grid item lg={12} textAlign={"center"}>
+          <Box
+            component={"img"}
+            src="/img/Nejat.png"
+            width="20vw"
+            height="16/9"
+          ></Box>
+        </Grid>
         <Grid
           item
-          xs={12}
-          md={6}
-          height="70svh"
-          component="img"
-          src="/img/s.jpg"
+          xs={11}
+          md={5}
+          sx={{
+            aspectRatio: 16 / 14,
+            backgroundImage: "url(/img/flood.jpg)",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         ></Grid>
-        <Grid item xs={12} md={6} padding="0px 20px">
-          <Typography variant="h4">Flood</Typography>
-
-          <Typography>
-            Floods, a force of nature that can both awe and devastate, have
-            shaped the landscapes and communities throughout history. Join us on
-            a journey to understand the immense power and impact of floods. Dive
-            into the science behind floods, learning about the intricate
-            mechanisms that lead to their occurrence. From heavy rainfall to
-            overflowing rivers and coastal storms, we unravel the factors that
-            contribute to these natural phenomena. Gain insights into the
-            hydrological processes that shape the flow of water and the complex
-            interactions with the environment. Explore the consequences of
-            floods, from their immediate effects on communities to their
-            long-term implications. Discover how floods can disrupt daily life,
-            damage infrastructure, and impact economies. Delve into the social
-            and psychological aspects, as communities rally together in the face
-            of adversity and demonstrate the utmost resilience. Uncover the
-            strategies and technologies used in flood management and mitigation.
-            Learn about innovative engineering solutions, early warning systems,
-            and adaptive measures that help communities prepare for and respond
-            to these natural disasters. Understand the importance of urban
-            planning, land management,
-          </Typography>
-        </Grid>
-      </Grid>
-
-      <Grid
-        container
-        margin="50px 0px"
-        sx={{
-          background: (theme) =>
-            `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.5)), url('/img/s.jpg')`,
-          backgroundAttachment: "fixed",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          color: "white",
-        }}
-      >
-        <Grid item xs={12} md={6} padding="0px 20px">
-          <Typography variant="h4">Flood</Typography>
-
-          <Typography>
-            Floods, a force of nature that can both awe and devastate, have
-            shaped the landscapes and communities throughout history. Join us on
-            a journey to understand the immense power and impact of floods. Dive
-            into the science behind floods, learning about the intricate
-            mechanisms that lead to their occurrence. From heavy rainfall to
-            overflowing rivers and coastal storms, we unravel the factors that
-            contribute to these natural phenomena. Gain insights into the
-            hydrological processes that shape the flow of water and the complex
-            interactions with the environment. Explore the consequences of
-            floods, from their immediate effects on communities to their
-            long-term implications. Discover how floods can disrupt daily life,
-            damage infrastructure, and impact economies. Delve into the social
-            and psychological aspects, as communities rally together in the face
-            of adversity and demonstrate the utmost resilience. Uncover the
-            strategies and technologies used in flood management and mitigation.
-            Learn about innovative engineering solutions, early warning systems,
-            and adaptive measures that help communities prepare for and respond
-            to these natural disasters. Understand the importance of urban
-            planning, land management,
-          </Typography>
-        </Grid>
-
         <Grid
           item
-          xs={12}
-          md={6}
-          height="70svh"
-          component="img"
-          src="/img/d.jpg"
-        ></Grid>
+          xs={11}
+          md={5}
+          padding="0px 20px"
+          sx={{ aspectRatio: 16 / 14 }}
+        >
+          <Typography variant="h5" color={"green"} fontWeight="bold">
+            About us
+          </Typography>
+          <Typography variant="h3" fontWeight="bold">
+            Welcome to <span style={{ color: "green" }}> NEJAT</span>
+          </Typography>
+          <Typography color={"text.secondary"} mt={1}>
+            {" "}
+            Nejat explore the impact of earthquakes, floods and more. Get
+            informed, prepared, and empowered. Join us in building resilience
+            for a safer future.
+          </Typography>
+          <Typography color="text.secondary" mt={4}>
+            Nejat Natural Disaster System: Unveiling Nature's Power Prepare.
+            Respond. Protect. Discover the tools and knowledge to mitigate the
+            impact of natural disasters. Real-time alerts, interactive maps, and
+            expert insights empower communities to stay informed and take
+            proactive measures. Join our community of resilience. Exchange
+            experiences, share best practices, and collaborate for a safer
+            future. Unlock the mysteries of nature's fury. Together, let's build
+            a more secure and resilient world.
+          </Typography>
+
+          <Grid container spacing={2} mt={1}>
+            <Grid item xs={6}>
+              {data.slice(0, Math.ceil(data.length / 2)).map((item) => (
+                <Grid
+                  key={item.id}
+                  display="flex"
+                  justifyContent={"sart"}
+                  alignItems={"center"}
+                >
+                  <ArrowForwardIcon sx={{ color: "green" }} />
+                  <Typography variant="h6" color="text.secondary">
+                    {item.title}
+                  </Typography>
+                </Grid>
+              ))}
+            </Grid>
+            <Grid item xs={6}>
+              {data.slice(Math.ceil(data.length / 2)).map((item) => (
+                <Grid key={item.id} display="flex">
+                  <ArrowForwardIcon sx={{ color: "green" }} />
+                  <Typography variant="h6" color="text.secondary">
+                    {item.title}
+                  </Typography>
+                </Grid>
+              ))}
+            </Grid>
+          </Grid>
+          <Button
+            variant="contained"
+            color="success"
+            sx={{ mt: 3, padding: 1 }}
+          >
+            Read More
+          </Button>
+        </Grid>
       </Grid>
     </Grid>
   );
 };
+
 export default Home;
