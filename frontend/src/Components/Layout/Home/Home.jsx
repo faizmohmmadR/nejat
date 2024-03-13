70351b5a3992245b671aa9edf4f5f0bbdc108bf4
Faiz Mohammad <faizmohammadramdel@gmail.com>
2024-03-13 06:03:14 +0430

    charts  added

--- frontend/src/Components/Layout/Home/Home.jsx
+++ frontend/src/Components/Layout/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Box, Button, Grid, Typography } from "@mui/material";
 import React from "react";
 import Services from "../Services/Services";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import Charts from "../Charts/Charts";
 
 const Home = () => {
   const data = [
@@ -122,6 +123,7 @@ const Home = () => {
         </Grid>
       </Grid>
       <Services />
+      <Charts />
     </Grid>
   );
 };
