a7800c7a812d5f8be598f5f093c8049fd182b178
Tahir <thair@gmail.com>
2024-03-13 15:48:43 +0430

    this page is created

--- frontend/src/Components/Layout/PopualerEvent/PopulerEvent.jsx
+++ frontend/src/Components/Layout/PopualerEvent/PopulerEvent.jsx
@@ -1,4 +1,4 @@
-import { Grid, Typography, Container } from "@mui/material";
+import { Grid, Typography, Container, Box } from "@mui/material";
 import React from "react";
 import { styled } from "@mui/system";
 
@@ -43,43 +43,40 @@ const PopulerEvent = () => {
               sx={{
                 transition: "all 1s",
                 ":hover": {
-                  transform: "scale(1.1,1.1)",
+                  transform: "scale(0.9,0.9)",
                 },
                 position: "relative",
               }}
             >
               <StyledImage src="img/pexels-ahmed-akacha-6471684.jpg" alt="" />
-              <Typography
-                variant="h6"
+              <Box
                 sx={{
-                  position: "absolute",
-                  top: "10%",
-                  left: "10%",
-
-                  color: "red",
                   backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
+                  color: "red",
                   fontWeight: "bold",
+                  position: "absolute",
+                  top: 0,
+                  start: 0,
+                  margin: "2rem",
+                  padding: "0.5rem 1rem",
                 }}
               >
-                20 people were killed
-              </Typography>
-              <Typography
-                variant="h6"
+                20 people
+              </Box>
+              <Box
                 sx={{
-                  position: "absolute",
-                  bottom: "10%",
-                  right: "10%",
-                  color: "black",
                   backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
+                  color: "primary.main",
                   fontWeight: "bold",
+                  position: "absolute",
+                  bottom: 0,
+                  end: 0,
+                  margin: "1rem",
+                  padding: "0.5rem 1rem",
                 }}
               >
                 Kandahar
-              </Typography>
+              </Box>
             </Grid>
             <Grid
               item
@@ -89,43 +86,40 @@ const PopulerEvent = () => {
               sx={{
                 transition: "all 1s",
                 ":hover": {
-                  transform: "scale(1.1,1.1)",
+                  transform: "scale(0.9,0.9)",
                 },
                 position: "relative",
               }}
             >
               <StyledImage src="img/pexels-david-henry-5440808.jpg" alt="" />
-              <Typography
-                variant="h6"
+              <Box
                 sx={{
-                  position: "absolute",
-                  top: "10%",
-                  left: "10%",
-
-                  color: "red",
                   backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
+                  color: "red",
                   fontWeight: "bold",
+                  position: "absolute",
+                  top: 0,
+                  start: 0,
+                  margin: "2rem",
+                  padding: "0.5rem 1rem",
                 }}
               >
-                18 people were killed
-              </Typography>
-              <Typography
-                variant="h6"
+                25 people
+              </Box>
+              <Box
                 sx={{
-                  position: "absolute",
-                  bottom: "10%",
-                  right: "10%",
-                  color: "red",
                   backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
+                  color: "primary.main",
                   fontWeight: "bold",
+                  position: "absolute",
+                  bottom: 0,
+                  end: 0,
+                  margin: "1rem",
+                  padding: "0.5rem 1rem",
                 }}
               >
-                Kabul
-              </Typography>
+                Helmand
+              </Box>
             </Grid>
             <Grid
               item
@@ -135,7 +129,7 @@ const PopulerEvent = () => {
               sx={{
                 transition: "all 1s",
                 ":hover": {
-                  transform: "scale(1.1,1.1)",
+                  transform: "scale(0.9,0.9)",
                 },
                 position: "relative",
               }}
@@ -144,37 +138,34 @@ const PopulerEvent = () => {
                 src="img/pexels-ridzuan-ibrahim-9739131.jpg"
                 alt=""
               />
-              <Typography
-                variant="h6"
+              <Box
                 sx={{
-                  position: "absolute",
-                  top: "10%",
-                  left: "10%",
-
+                  backgroundColor: "white",
                   color: "red",
-                  backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
                   fontWeight: "bold",
-                }}
-              >
-                48 people were killed
-              </Typography>
-              <Typography
-                variant="h6"
-                sx={{
                   position: "absolute",
-                  bottom: "10%",
-                  right: "10%",
-                  color: "black",
-                  backgroundColor: "white",
-                  fontFamily: "sans-serif",
-                  padding: "20px",
-                  fontWeight: "bold",
+                  top: 0,
+                  start: 0,
+                  margin: "2rem",
+                  padding: "0.5rem 1rem",
                 }}
               >
-                Ghaznai
-              </Typography>
+                23 people
+              </Box>
+              <Box
+                sx={{
+                  backgroundColor: "white",
+                  color: "primary.main",
+                  fontWeight: "bold",
+                  position: "absolute",
+                  bottom: 0,
+                  end: 0,
+                  margin: "1rem",
+                  padding: "0.5rem 1rem",
+                }}
+              >
+                Kabul
+              </Box>
             </Grid>
           </Grid>
         </Grid>
@@ -187,43 +178,40 @@ const PopulerEvent = () => {
           sx={{
             transition: "all 1s",
             ":hover": {
-              transform: "scale(1.1,1.1)",
+              transform: "scale(0.9,0.9)",
             },
             position: "relative",
           }}
         >
           <StyledImage src="img/pexels-алесь-усцінаў-11734705.jpg" alt="" />
-          <Typography
-            variant="h6"
+          <Box
             sx={{
-              position: "absolute",
-              top: "10%",
-              left: "10%",
-
+              backgroundColor: "white",
               color: "red",
-              backgroundColor: "white",
-              fontFamily: "sans-serif",
-              padding: "20px",
               fontWeight: "bold",
-            }}
-          >
-            37 people were killed
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
               position: "absolute",
-              bottom: "10%",
-              right: "10%",
-              color: "black",
-              backgroundColor: "white",
-              fontFamily: "sans-serif",
-              padding: "20px",
-              fontWeight: "bold",
+              top: 0,
+              start: 0,
+              margin: "2rem",
+              padding: "0.5rem 1rem",
             }}
           >
-            Mazara-Shrif
-          </Typography>
+            14 people
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "white",
+              color: "primary.main",
+              fontWeight: "bold",
+              position: "absolute",
+              bottom: 0,
+              end: 0,
+              margin: "1rem",
+              padding: "0.5rem 1rem",
+            }}
+          >
+            Badakhshan
+          </Box>
         </Grid>
       </Grid>
     </Container>
