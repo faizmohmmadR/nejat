2442eb6e987e5eef749a49c0100718f6ddeea965
Faiz Mohammad <faizmohammadramdel@gmail.com>
2024-03-13 22:59:39 +0430

    footer error solved

--- frontend/src/Components/Layout/Home/Home.jsx
+++ frontend/src/Components/Layout/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import Services from "../Services/Services";
 
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Charts from "../Charts/Charts";
 import News from "../News/News";
+import Charts from "../Charts/Charts";
 
 const Home = () => {
   const data = [
@@ -125,10 +125,16 @@ const Home = () => {
               ))}
             </Grid>
           </Grid>
+
           <Button
             variant="contained"
-            color="success"
-            sx={{ mt: 3, padding: 1 }}
+            sx={{
+              mt: 3,
+              padding: "5px 20px",
+              zIndex: 0,
+              bgcolor: "#7814c9",
+              ":hover": { bgcolor: "#a141f0" },
+            }}
           >
             Read More
           </Button>
