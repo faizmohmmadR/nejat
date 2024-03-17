1efab45fd5b6c3d2a20439ccc657c989fd40c1d8
Tahir <thair@gmail.com>
2024-03-17 09:50:44 +0430

    this image section is done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import { Outlet } from "react-router-dom";
 import { Grid } from "@mui/material";
 import Footer from "./Components/Layout/Footer/Footer";
 import Nav from "./Components/Layout/Nav/Nav";
+import PopulerEvent from "./Components/Layout/PopulerEvent/PopulerEvent";
 
 function App() {
   return (
     <Grid maxWidth="xl" margin="0px auto" container>
       <Nav />
       <Outlet />
+      <PopulerEvent />
       <Footer />
     </Grid>
   );
