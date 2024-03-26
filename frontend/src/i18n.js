cc875062deb13069228e7fa05a08435e53307a54
Tahir <thair@gmail.com>
2024-03-26 23:49:48 +0430

    contact page localization is Done

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -2,22 +2,24 @@ import i18n from "i18next";
 import languagedetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
-import home from "./Components/locales/en/home.json";
+
 // import home from './Components/locales/ps/home.json'
 
 const resources = {
   en: {
     home: require("./Components/locales/en/home.json"),
     events: require("./Components/locales/en/events.json"),
-
+    contact: require("./Components/locales/en/contact.json"),
   },
   ps: {
     home: require("./Components/locales/ps/home.json"),
     events: require("./Components/locales/ps/events.json"),
+    contact: require("./Components/locales/ps/contact.json"),
   },
   fa: {
     home: require("./Components/locales/fa/home.json"),
     events: require("./Components/locales/fa/events.json"),
+    contact: require("./Components/locales/fa/contact.json"),
   },
 };
 
@@ -29,7 +31,7 @@ i18n
     debug: true,
     fallbackLng: "en",
     returnObjects: true,
-    ns: ["home","events"],
+    ns: ["home", "events", "contact"],
     lng: "en",
     resources,
     interpolation: {
